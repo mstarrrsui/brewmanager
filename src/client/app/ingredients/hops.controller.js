b614f999415535b4d4875595425b2aa10c30d93d
Mike Starr <mstarr@rsui.com>
2016-04-20 15:19:01 -0400

    paging in the table

--- src/client/app/ingredients/hops.controller.js
+++ src/client/app/ingredients/hops.controller.js
@@ -9,21 +9,48 @@
     /* @ngInject */
     function HopsController($http, dataservice, logger) {
         var vm = this;
+        vm.sortorder = 'countryOfOrigin';
+        vm.hopFilter = '';
         vm.hops = [];
+        vm.currPageData = [];
         vm.title = 'Hops';
+        vm.currPage;
+        vm.pageSize = 20;
+        vm.totalPages;
         vm.clickMe = clickMe;
+        vm.nextPage = nextPage;
+        vm.prevPage = prevPage;
 
         activate();
 
         function activate() {
             return getHops().then(function() {
+                setCurrPageData(1);
                 logger.info('Activated Hops View');
             });
         }
 
+        function setCurrPageData(page) {
+            vm.currPage = page;
+            vm.currPageData = _.slice(vm.hops,
+                ((page - 1) * vm.pageSize),
+                (page * vm.pageSize));
+        }
+
+        function prevPage() {
+            setCurrPageData(vm.currPage - 1);
+        }
+
+        function nextPage() {
+            setCurrPageData(vm.currPage + 1);
+        }
+
+
+
         function getHops() {
             return dataservice.getHops().then(function(data) {
                 vm.hops = data;
+                vm.totalPages = vm.hops.length / vm.pageSize;
                 return vm.hops;
             });
         }
